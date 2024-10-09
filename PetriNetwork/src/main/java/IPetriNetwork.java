02968befcdaab3da9492a324c81721ba3effb51e
Anamaria Miranda <anamaria.miranda-rodriguez@imt-atlantique.net>
2024-10-09 19:07:06 +0200

    Added additional methods in petri interface

--- PetriNetwork/src/main/java/IPetriNetwork.java
+++ PetriNetwork/src/main/java/IPetriNetwork.java
@@ -1,26 +1,29 @@
-
+package main.java;
 
 import java.util.List;
 
 public interface IPetriNetwork {
-	
+
 	public void addPlace(String label);
+	public void addPlace(String label, int tokens);
 	public void rmPlace(String label);
 	public void setPlaceToken(String label, int token);
+	public void showPlaces();
 	
 	public void addTransition(String label);
 	public void rmTransition(String label);
+	public void showTransitions();
 	
-	public void addInArc(String label, String fromTransition, String toPlace);
-	public void addOutArc(String label, String fromPlace, String toTransition);
+	public void addArc(String label, String transition, String place, String type);
+	public void addArc(String label, String transition, String place, String type, int weight);
+	public void modArc(String label, String transition, String place, String type);
+
 	public void setArcWeight(String label, int weight);
 	public void rmArc(String label);
-	
+	public void showArcs();
+
 	public void fire(String label);
 	public List<String> fireableTransitions();
-	
-	
-	
-	
+	public void showAllElements();
 
 }
